294e5044de8ce3bddb2e597bcb53c11a69ab9fe0
Dang <97234801+dangno1@users.noreply.github.com>
2023-09-22 16:43:14 +0700

    master

--- src/scss/script.js
+++ src/scss/script.js
@@ -13,7 +13,6 @@ function closeNav1() {
 }
 
 const header = document.querySelector("header");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     header.classList.add("active");
@@ -21,3 +20,33 @@ window.addEventListener("scroll", () => {
     header.classList.remove("active");
   }
 });
+
+function boxGreen() {
+  const boxGreen = document.querySelector(".setups__time-box");
+  const price = document.querySelector(".price");
+  const priceTwo = document.querySelector(".price-2");
+
+  if (boxGreen.classList.contains("moving")) {
+    boxGreen.classList.remove("moving");
+    price.innerHTML = "19";
+    priceTwo.innerHTML = "49";
+  } else {
+    boxGreen.classList.add("moving");
+    price.innerHTML = "15.2";
+    priceTwo.innerHTML = "39.8";
+  }
+}
+
+function bannerBox() {
+  const bannerBox = document.querySelector(".banner__bottom-box");
+  const bannerText = document.querySelector(".banner__bottom-text");
+
+  if (bannerBox.classList.contains("bg-green")) {
+    bannerBox.classList.remove("bg-green");
+    bannerText.innerHTML = "How It Works";
+  } else {
+    bannerBox.classList.add("bg-green");
+    bannerText.innerHTML = "It Works";
+  }
+}
+
